2f1917a2eff0a4a7ad793ab65ae8cbe3ffbd5f43
dvbeko <90577875+dvbeko@users.noreply.github.com>
2021-09-17 16:14:58 +0200

    bullet rotation

--- ProjectTestEnvironment/Assets/Scripts/EnemyBullet.cs
+++ ProjectTestEnvironment/Assets/Scripts/EnemyBullet.cs
@@ -16,6 +16,12 @@ public class EnemyBullet : MonoBehaviour
         Vector2 _moveDirection = (_target.transform.position - transform.position).normalized * _speed;
         _rigidbody.velocity = new Vector2(_moveDirection.x, _moveDirection.y);
         Destroy(this.gameObject, 2);
+
+        transform.eulerAngles = new Vector3(0, 0, Vector2ToAngle(_moveDirection) - 90f);
     }
 
+    public static float Vector2ToAngle(Vector2 vect)
+    {
+        return Mathf.Atan2(vect.y, vect.x) * Mathf.Rad2Deg;
+    }
 }
