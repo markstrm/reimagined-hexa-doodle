3492aea7b8b53b2cb34a68f70cb367bab6291420
markstrm <markus.bergstrom@outlook.com>
2021-09-17 14:57:05 +0200

    My changes

--- ProjectTestEnvironment/Assets/Scripts/PlayerMovement.cs
+++ ProjectTestEnvironment/Assets/Scripts/PlayerMovement.cs
@@ -25,8 +25,9 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        transform.localPosition += Vector3.right * speed * Time.deltaTime * movement.x;
-        transform.localPosition += Vector3.up * speed * Time.deltaTime* movement.y;
+        transform.localPosition += Vector3.right * speed * movement.x;
+        transform.localPosition += Vector3.up * speed * movement.y;
+
 
     }
 
