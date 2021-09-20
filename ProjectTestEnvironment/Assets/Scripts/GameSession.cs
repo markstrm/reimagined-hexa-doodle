e6a97e74a0ea4fb003f14a894acce49b3ac0804d
markstrm <markus.bergstrom@outlook.com>
2021-09-20 13:52:04 +0200

    enemy fix

--- ProjectTestEnvironment/Assets/Scripts/GameSession.cs
+++ ProjectTestEnvironment/Assets/Scripts/GameSession.cs
@@ -7,9 +7,10 @@ public class GameSession : MonoBehaviour
 
     int score = 0;
     public int _lives = 3;
-    public float respawnTime = 2.0f;
+    public float _respawnTime = 2.0f;
+    public float _respawnInvulnerabilityTime = 3.0f;
 
-    public GameObject player;
+    public PlayerMovement player;
 
     public void PlayerDied()
     {
@@ -21,19 +22,35 @@ public class GameSession : MonoBehaviour
         }
         else
         {
-            Invoke(nameof(Respawn), this.respawnTime);
+            Invoke(nameof(Respawn), this._respawnTime);
         }
       
     }
 
     private void Respawn()
     {
+
         this.player.transform.position = Vector3.zero; //spawns the player at the center of the board
+        this.player.gameObject.layer = LayerMask.NameToLayer("Ignore Collisions"); //when player respawns, temporarily change layer to ignore all collisions
         this.player.gameObject.SetActive(true);
+        
+        Invoke(nameof(TurnOnCollisions), this._respawnInvulnerabilityTime); //3s after spawning, set the layer back to player to enable collisions
+    }
+
+    private void TurnOnCollisions()
+    {
+        this.player.gameObject.layer = LayerMask.NameToLayer("Player");
     }
 
     private void GameOver()
     {
+        //here we can place gameover text
+        //score text etc - press X to play again
+        //destroy all other game objects??
+        this._lives = 3;
+        this.score = 0;
+
+        Invoke(nameof(Respawn), this._respawnTime);
         //later
     }
 
