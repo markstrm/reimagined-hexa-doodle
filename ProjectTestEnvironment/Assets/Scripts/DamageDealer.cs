6d1fa0b4d706208f316cef65a58e9ed43394ea77
dvbeko <90577875+dvbeko@users.noreply.github.com>
2021-09-23 15:23:00 +0200

    Ion burst

--- ProjectTestEnvironment/Assets/Scripts/DamageDealer.cs
+++ ProjectTestEnvironment/Assets/Scripts/DamageDealer.cs
@@ -7,6 +7,9 @@ public class DamageDealer : MonoBehaviour
 {
 
     [SerializeField] int damage = 100;
+    [SerializeField] AudioClip _ionBurstSFX;
+    [SerializeField] float _ionBurstSFXVol;
+    [SerializeField] ParticleSystem _burstVFX;
 
     public int GetDamage()
     {
@@ -16,5 +19,10 @@ public class DamageDealer : MonoBehaviour
     public void Hit()
     {
        Destroy(gameObject);
+        if (gameObject.tag == "Bullet")
+        {
+            var explosion = Instantiate(_burstVFX, transform.position, transform.rotation);//ion burst vfx
+            AudioSource.PlayClipAtPoint(_ionBurstSFX, transform.position, _ionBurstSFXVol);
+        }
     }
 }
