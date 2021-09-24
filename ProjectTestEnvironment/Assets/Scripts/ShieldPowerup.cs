e514e9af860d654b682940193a5da23c030f69dc
Andreas Hansen <Andreas.Hansen@futuregames.nu>
2021-09-24 16:15:55 +0200

    Shield Powerup

---
+++ ProjectTestEnvironment/Assets/Scripts/ShieldPowerup.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldPowerup : MonoBehaviour
+{
+    public PlayerMovement player;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+    private void OnTriggerEnter2D(Collider2D other) // If Player collides with powerup
+    {
+
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("More Shields");
+            Pickup(other.gameObject.GetComponent<PlayerMovement>());
+        }
+
+    }
+
+    void Pickup(PlayerMovement player) // the powerup speed buff given to the player and also starts a timer in the Playermovement script. Also destroys the powerup object
+    {
+
+        player._health = 300;
+
+
+
+        Destroy(gameObject);
+
+    }
+}
