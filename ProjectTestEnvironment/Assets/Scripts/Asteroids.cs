584cf45d4184df19e58627b26eaf86bcdc82feae
markstrm <markus.bergstrom@outlook.com>
2021-09-26 21:04:27 +0200

    Added some asteroids to the minimap as well and gave asteroids some rotation

---
+++ ProjectTestEnvironment/Assets/Scripts/Asteroids.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroids : MonoBehaviour
+{
+
+    private float _Rotation;
+    public float _RotationSpeed;
+    public bool _ClockwiseRotation;
+
+    private Rigidbody2D _Rigidbody;
+
+
+    private void Awake()
+    {
+        _Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (_ClockwiseRotation == false)
+        {
+            _Rotation += Time.deltaTime * _RotationSpeed;
+        }
+        else
+        {
+            _Rotation += -Time.deltaTime * _RotationSpeed;
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, _Rotation);
+
+    }
+
+
+
+}
